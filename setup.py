23b2c6812091dfbf465c65dee4c0bdfa0ff3d7e9
Meenakshi Sundaram <vms20591@gmail.com>
2016-07-09 14:30:31 +0530

    Add setup.py for installation

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='ZerconfRegisterService',
+    version='0.1',
+    author='Meenakshi Sundaram',
+    author_email='vms20591@gmail.com',
+    packages=['ZeroconfRegisterService'],
+    url='https://github.com/vms20591/ZeroconfRegisterService',
+    description='A simple module for services to register themselves with Zeroconf for easy advertisement',
+    license='GPL V3',
+    long_description=open('README.md').read(),
+    extras_require={
+        "netifaces": ["netifaces"],
+        "zeroconf": ["zeroconf"],
+    },
+)
